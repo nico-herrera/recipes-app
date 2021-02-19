67241293debf609b69d0470c39bce23422d3d72a
Nico Herrera <nherrera0831@gmail,com>
2021-02-18 21:40:50 -0800

    app takes data and sets to local storage, and displays

---
+++ client/src/components/Home.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from "react";
+import { recipes } from "../data/dummyData";
+
+const Home = () => {
+  const [recipe, setRecipe] = useState({
+    name: "",
+    ingredients: "",
+    instructions: "",
+  });
+  const [recipeList, setRecipeList] = useState(recipes);
+
+  const changeHandler = (e) => {
+    setRecipe({ ...recipe, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setRecipeList([...recipeList, recipe]);
+    localStorage.setItem("recipe", JSON.stringify([...recipeList, recipe]));
+  };
+
+  useEffect(() => {
+    setRecipeList(JSON.parse(localStorage.getItem("recipe")));
+  }, []);
+
+  console.log(recipeList);
+  return (
+    <div>
+      <h1>DIY Recipes!</h1>
+      <h3>Lets practice! Try adding a recipe below</h3>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="name">Recipe Name</label>
+        <input
+          placeholder="Cheese Pizza"
+          id="name"
+          name="name"
+          value={recipe.name}
+          onChange={changeHandler}
+        />
+        <label>Ingredients</label>
+        <input
+          type="ingredients"
+          placeholder="Cheese, duh!"
+          id="ingredients"
+          name="ingredients"
+          value={recipe.ingredients}
+          onChange={changeHandler}
+        />
+        <input
+          type="instructions"
+          placeholder="Cook it?"
+          id="instructions"
+          name="instructions"
+          value={recipe.instructions}
+          onChange={changeHandler}
+        />
+        <button>Log in</button>
+      </form>
+      {recipeList?.map((item) => (
+        <div>
+          <h2>{item.name}</h2>
+          <p>{item.ingredients}</p>
+          <p>{item.instructions}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Home;
---
+++ client/src/data/dummyData.js
@@ -0,0 +1,12 @@
+export const recipes = [
+  {
+    name: "Mac and Cheese",
+    ingredients: "Cheese, milk, noodles",
+    instructions: "Cook for 10 minutes.",
+  },
+  {
+    name: "Grilled Cheese",
+    ingredients: "Cheese and bread",
+    instructions: "Cook for 3 minutes.",
+  },
+];
